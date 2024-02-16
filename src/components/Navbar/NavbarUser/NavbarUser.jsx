e61e719c5f1a2f876fde73368aca611256aa2d1f
SerhiiMW <137871447+SerhiiMW@users.noreply.github.com>
2024-02-16 17:09:06 +0200

    homework-8

--- src/components/Navbar/NavbarUser/NavbarUser.jsx
+++ src/components/Navbar/NavbarUser/NavbarUser.jsx
@@ -7,8 +7,8 @@ import { selectUser } from "../../../redux/auth/auth-selectors";
 import styles from "./navbar-user.module.css";
 
 const NavbarUser = ()=> {
-    // const {name} = useSelector(selectUser);
-    const name = useSelector(selectUser);
+    const {name} = useSelector(selectUser);
+    // const name = useSelector(selectUser);
     const dispatch = useDispatch();
 
     const onLogout = ()=> dispatch(logout());
